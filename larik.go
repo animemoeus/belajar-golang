b13fdd3297a13e35adc4ba8d3afc29147f52c8ef
Arter Tendean <arter@samb.co.id>
2024-10-02 14:34:13 +0700

    Larik

---
+++ larik.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	var names [3]string
+	names[0] = "Alpha"
+	names[1] = "Beta"
+	names[2] = "Gamma"
+
+	names[1] = "Delta"
+
+	fmt.Println(names)
+	fmt.Println(len(names))
+	fmt.Println(names[0])
+	fmt.Println(names[1])
+	fmt.Println(names[2])
+
+	//	create array directly
+	var values = [3]int{1, 3}
+	fmt.Println(values)
+	fmt.Println(values[0])
+	fmt.Println(values[1])
+	fmt.Println(values[2])
+
+}
