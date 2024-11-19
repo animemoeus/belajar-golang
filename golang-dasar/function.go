2627a208a57c3e237fe521b3069be10e9dd74966
Arter Tendean <arter@samb.co.id>
2024-11-19 11:49:19 +0700

    Update function

--- golang-dasar/function.go
+++ golang-dasar/function.go
@@ -6,6 +6,24 @@ func sayHello() {
 	fmt.Println("halo dunia")
 }
 
+func sayHelloTo(firstName string, lastName string) {
+	fmt.Println("Hello", firstName, lastName)
+}
+
+func getHello(name string) string {
+	return "Hello " + name
+}
+
+func getFullName(firstName string, lastName string) (string, string) {
+	return firstName, lastName
+}
+
 func main() {
-	sayHello()
+	//sayHello()
+	//sayHelloTo("arter", "tendean")
+	hello := getHello("arter tendean")
+	fmt.Println(hello)
+
+	firstName, lastName := getFullName("arter", "tendean")
+	fmt.Println(firstName, lastName)
 }
