64fa9bb116a976d5ae014a1e60cd171951a7353d
Arter Tendean <arter@samb.co.id>
2024-11-22 12:10:12 +0700

    Adding math

---
+++ standard-library/math.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println(math.Abs(-50))
+	fmt.Println(math.Ceil(1.50))
+	fmt.Println(math.Floor(1.50))
+	fmt.Println(math.Round(1.50))
+	fmt.Println(math.Max(1, 2))
+}
