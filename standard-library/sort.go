33d14e49cacc5aad99021c4b41197cea188d937f
Arter Tendean <arter@samb.co.id>
2024-11-22 13:20:34 +0700

    Adding sort

---
+++ standard-library/sort.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type User struct {
+	Name string
+	Age  int
+}
+
+type UserSlice []User
+
+func (s UserSlice) Len() int {
+	return len(s)
+}
+
+func (s UserSlice) Less(i, j int) bool {
+	return s[i].Age < s[j].Age
+}
+
+func (s UserSlice) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func main() {
+	users := []User{{"Arter", 24}, {"Takagi", 15}, {"Angie", 19}}
+	sort.Sort(UserSlice(users))
+	fmt.Println(users)
+}
